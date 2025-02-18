2fcfdc1d9fff407b44523a055b90c07acf9199f5
nazim1971 <md.nazimuddinaj@gmail.com>
2025-02-18 21:47:25 +0600

    add img

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,39 +1,49 @@
-import { Card, CardContent } from "../ui/card";
 import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "../ui/carousel";
-
-const Hero = () => {
-  return (
-    <div className="relative w-full">
-      {/* Responsive Carousel */}
-      <Carousel className="w-full h-[300px] md:h-[400px] lg:h-[500px]">
-        <CarouselContent>
-          {Array.from({ length: 5 }).map((_, index) => (
-            <CarouselItem key={index}>
-              <div className="p-1">
-                <Card className="h-full">
-                  <CardContent className="flex items-center justify-center p-6">
-                    <span className="text-2xl md:text-3xl lg:text-4xl font-semibold">
-                      {index + 1}
-                    </span>
-                  </CardContent>
-                </Card>
-              </div>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-
-        {/* Navigation Controls */}
-        <CarouselPrevious className="absolute top-1/2 left-4 transform -translate-y-1/2" />
-        <CarouselNext className="absolute top-1/2 right-4 transform -translate-y-1/2" />
-      </Carousel>
-    </div>
-  );
-};
-
-export default Hero;
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+  } from "../ui/carousel";
+  import Image from "next/image";
+  
+  const Hero = () => {
+    const images = [
+      "https://res.cloudinary.com/dfvgxf4dc/image/upload/v1738599111/pytkaypz223zdhp7b62a.avif",
+      "https://res.cloudinary.com/dfvgxf4dc/image/upload/v1738599041/foafvixt90gqfqzwf1gw.webp",
+      "https://res.cloudinary.com/dfvgxf4dc/image/upload/v1738599200/b5frlcoxq6tuadpepvmo.png",
+      "https://res.cloudinary.com/dfvgxf4dc/image/upload/v1738599156/rt5a9vu6lsewdfphyzvi.avif",
+      "https://res.cloudinary.com/dfvgxf4dc/image/upload/v1738606127/dpwklji14tro6xwc4ubo.jpg",
+    ];
+  
+    return (
+      <div className="relative w-full">
+        {/* Responsive Carousel */}
+        <Carousel className="w-full h-[300px] md:h-[400px] lg:h-[500px]">
+          <CarouselContent className="h-[300px] md:h-[400px] lg:h-[500px]">
+            {images.map((src, index) => (
+              <CarouselItem key={index}>
+                <div className="relative w-full h-full">
+                  {/* Image with layout fill */}
+                  <Image
+                    src={src}
+                    alt={`Slide ${index + 1}`}
+                    layout="fill"
+                    objectFit="cover"
+                    className="rounded-lg"
+                  />
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+  
+          {/* Navigation Controls */}
+          <CarouselPrevious className="absolute top-1/2 left-4 transform -translate-y-1/2" />
+          <CarouselNext className="absolute top-1/2 right-4 transform -translate-y-1/2" />
+        </Carousel>
+      </div>
+    );
+  };
+  
+  export default Hero;
+  
